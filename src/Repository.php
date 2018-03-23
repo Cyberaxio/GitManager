abedc3b7d6aae847a70f9de1bbf402e6733ffde0
Neo Icari <neoscopia@gmail.com>
2018-03-23 17:00:21 +0100

    Fix value

--- src/Repository.php
+++ src/Repository.php
@@ -80,7 +80,7 @@ class Repository
 		if(is_array($key)){
 			$this->config = array_merge($this->config, $key);
 		}else{
-			$this->config[$key] = $value =! null ? $value : $this->config[$key];
+			$this->config[$key] = ($value ? $value : $this->config[$key]);
 		}
 		return $this;
 	}
